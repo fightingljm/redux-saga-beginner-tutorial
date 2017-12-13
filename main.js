ac5571b897fe5198dff915d341495f290316eb7f
wolf_liu <1321839748@qq.com>
2017-12-13 09:42:29 +0800

    no message

--- main.js
+++ main.js
@@ -3,22 +3,30 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+//引入 ./sagas 模块中的 Saga
+import { helloSaga, watchIncrementAsync } from './sagas'
 
 import Counter from './Counter'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const store = createStore(
+    reducer,
+    applyMiddleware(createSagaMiddleware(helloSaga,watchIncrementAsync))
+)
 
 const action = type => store.dispatch({type})
 
 function render() {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
-    document.getElementById('root')
-  )
+    ReactDOM.render(
+        <Counter
+            value={store.getState()}
+            onIncrement={() => action('INCREMENT')}
+            onDecrement={() => action('INCREMENT_ASYNC')}
+        />,
+        document.getElementById('root')
+    )
 }
 
 render()
